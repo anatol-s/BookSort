936e0b71d0ce7c33dc20d6d487b271f680513eee
Anatoliy Samokisha <a.samokisha@yandex.ru>
2015-10-20 22:48:58 +0300

    Add book

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,13 +10,12 @@ public class Main {
     private static void start() throws ParseException {
         List<Book> books = new LinkedList<Book>();
         books.add(new Book("Java 8. Полное руководство, 9-е издание.", "Герберт Шилдт.", 2015));
-        books.add(new Book("Java 8. Полное руководство, 9-е издание.", "Герберт Шилдт.", 2014));
         books.add(new Book("Java 7. Руководство для начинающих, 5-е издание.", "Герберт Шилдт.", 2015));
         books.add(new Book("Java 8. Карманный справочник.", "Роберт Лигуори, Патриция Лигуори.", 2015));
         books.add(new Book("Философия Java. 4-е полное издание.", "Брюс Эккель.", 2015));
-        books.add(new Book("Java 8. Аолное руководство, 9-е издание.", "Герберт Шилдт.", 2014));
+        books.add(new Book("Java. Методы программирования.", "Игорь Блинов, Валерий Романчик.", 2013));
 
-        Book.sort(books);
+        BookSort.sort(books);
 
         for (Book book : books) {
             System.out.println(book);
